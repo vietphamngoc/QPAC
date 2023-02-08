59ccab2dbebdf18346c4d5eadc9977348091b1f8
vietphamngoc <viphamngoc@gmail.com>
2023-02-08 11:43:35 +0000

    Added update strat for parity functions

--- update_strategy.py
+++ update_strategy.py
@@ -2,7 +2,7 @@ import utility as util
 
 from tnn import TNN
 
-def get_updates(network: TNN, errors: list)->list:
+def get_updates(measurements: dict, **kwargs)->list:
     """
     Function implementing the update strategy to learn the target concept.
 
@@ -13,26 +13,28 @@ def get_updates(network: TNN, errors: list)->list:
     Returns:
         A list of gates to be switched
     """
+    network = kwargs['network']
     n = network.dim
-    ones = util.str_to_ones(errors[0])
-    to_update = [ones]
+    errors = []
+    for l in measurements['errors']:
+        errors += l
+    to_update = [errors[0]]
     active = [util.str_to_ones(k) for k,v in network.gates.items() if v == 1]
 
     if len(active) > 1 and ["0"*n] not in active:
         return(["0"*n])
 
     for error in errors:
-        ones = util.str_to_ones(error)
-        if ones == set():
+        if error == set():
             return(["0"*n])
         all_empty = True
         for i in range(len(to_update)):
-            inter = ones.intersection(to_update[i])
+            inter = error & to_update[i]
             if inter != set():
                 to_update[i] = inter
                 all_empty = False
         if all_empty == True:
-            to_update.append(ones)
+            to_update.append(errors)
 
     additional_gates = []
     for g in to_update:
@@ -46,4 +48,28 @@ def get_updates(network: TNN, errors: list)->list:
                         return(["0"*n])
     to_update = to_update + additional_gates
 
-    return([util.ones_to_str(k, n) for k in to_update])
+    return(to_update)
+
+
+def get_parity_updates(measurements: dict, **kwargs)->list:
+    number = kwargs['group']
+    to_update = measurements['errors'][1]
+
+    def compare_and_update(l1, l2):
+        for a in l1:
+            for b in l2:
+                if a < b:
+                    c = b - a
+                    if c not in to_update:
+                        to_update.append(c)
+
+    for i in range(1, number):
+        compare_and_update(measurements['errors'][i], measurements['corrects'][i+1])
+        compare_and_update(measurements['errors'][-1-i], measurements['corrects'][-i])
+        compare_and_update(measurements['corrects'][i], measurements['errors'][i+1])
+        compare_and_update(measurements['corrects'][-1-i], measurements['errors'][-i])
+    
+    return(to_update)
+
+    
+
