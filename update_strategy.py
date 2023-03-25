71491ab47042f55667efd51a4665727aee18a046
vietphamngoc <viphamngoc@gmail.com>
2023-03-25 17:13:43 +0000

    Cleaner parity update

--- update_strategy.py
+++ update_strategy.py
@@ -1,7 +1,5 @@
 import utility as util
 
-from tnn import TNN
-
 def get_updates(measurements: dict, **kwargs)->list:
     """
     Function implementing the update strategy to learn the target concept.
@@ -14,45 +12,59 @@ def get_updates(measurements: dict, **kwargs)->list:
         A list of gates to be switched
     """
     network = kwargs['network']
-    n = network.dim
-    errors = []
-    for l in measurements['errors']:
-        errors += l
-    to_update = [errors[0]]
+    
     active = [util.str_to_ones(k) for k,v in network.gates.items() if v == 1]
 
-    if len(active) > 1 and ["0"*n] not in active:
-        return(["0"*n])
+    def get_intersections(meas):
+        collection = []
+        for l in meas:
+            collection += l
 
-    for error in errors:
-        if error == set():
-            return(["0"*n])
-        all_empty = True
-        for i in range(len(to_update)):
-            inter = error & to_update[i]
-            if inter != set():
-                to_update[i] = inter
-                all_empty = False
-        if all_empty == True:
-            to_update.append(errors)
+        if collection != []:
+            intersections = [collection[0]]
+            for s in collection[1:]:
+                all_empty = True
+                for i in range(len(intersections)):
+                    inter = s & intersections[i]
+                    if inter != set():
+                        intersections[i] = inter
+                        all_empty = False
+                if all_empty:
+                    intersections.append(s)
+            return(intersections)
+        else:
+            return([])
+    
+    errors_inter = get_intersections(measurements['errors'])
+    corrects_inter = get_intersections(measurements['corrects'])
 
-    additional_gates = []
-    for g in to_update:
-        if network.gates[util.ones_to_str(g, n)] == 0:
-            for a in active:
-                if g.intersection(a) != set():
-                    if g.intersection(a) == g:
-                        if a not in additional_gates:
-                            additional_gates.append(a)
-                    else:
-                        return(["0"*n])
-    to_update = to_update + additional_gates
+    to_update = []
+    add_0 = False
 
+    if len(errors_inter) == 1:
+        to_update = errors_inter
+    elif len(corrects_inter) == 1:
+        to_update = corrects_inter
+        add_0 = True
+    
+    if to_update != []:
+        for g in active:
+            if g != set():
+                inter = to_update[0] & g
+                if inter != g:
+                    to_update[0] = inter
+                    to_update.append(g)
+                else:
+                    to_update = []
+
+    if add_0:    
+        to_update.append(set())
+    
     return(to_update)
-
+    
 
 def get_parity_updates(measurements: dict, **kwargs)->list:
-    number = kwargs['group']
+    l = len(measurements['errors'])
     to_update = measurements['errors'][1]
 
     def compare_and_update(l1, l2):
@@ -63,11 +75,9 @@ def get_parity_updates(measurements: dict, **kwargs)->list:
                     if c not in to_update:
                         to_update.append(c)
 
-    for i in range(1, number):
+    for i in range(1, l-1):
         compare_and_update(measurements['errors'][i], measurements['corrects'][i+1])
-        compare_and_update(measurements['errors'][-1-i], measurements['corrects'][-i])
         compare_and_update(measurements['corrects'][i], measurements['errors'][i+1])
-        compare_and_update(measurements['corrects'][-1-i], measurements['errors'][-i])
     
     return(to_update)
 
